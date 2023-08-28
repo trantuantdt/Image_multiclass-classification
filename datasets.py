09c3e1f2a0dfd55cacaaa65cd18ee53357d32502
Tuan <trantuantdt@gmail.com>
2023-08-28 14:45:49 +0700

    dd

---
+++ datasets.py
@@ -0,0 +1,51 @@
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from torch.utils.data import DataLoader
+
+# batch size
+BATCH_SIZE = 64
+
+# the training transforms
+train_transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+    transforms.RandomRotation(degrees=(30, 70)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.5, 0.5, 0.5],
+        std=[0.5, 0.5, 0.5]
+    )
+])
+# the validation transforms
+valid_transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.5, 0.5, 0.5],
+        std=[0.5, 0.5, 0.5]
+    )
+])
+
+# training dataset
+train_dataset = datasets.ImageFolder(
+    root='input/butterflies_rev2/train',
+    transform=train_transform
+)
+# validation dataset
+valid_dataset = datasets.ImageFolder(
+    root='input/butterflies_rev2/valid',
+    transform=valid_transform
+)
+# training data loaders
+train_loader = DataLoader(
+    train_dataset, batch_size=BATCH_SIZE, shuffle=True,
+    num_workers=0, pin_memory=True
+)
+# validation data loaders
+valid_loader = DataLoader(
+    valid_dataset, batch_size=BATCH_SIZE, shuffle=False,
+    num_workers=0, pin_memory=True
+)
